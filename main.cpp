ba4d5336e866f2caf8c7d43de939861cb045b861
a01831645-cell <a01831645@tec.mx>
2026-04-20 21:23:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int expectedSum = n * (n + 1) / 2; 
-        int actualSum = 0;
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> valid(bank.begin(), bank.end());
 
-        for (int num : nums) {
-            actualSum += num;
+        if (!valid.count(endGene)) return -1;
+        
+        queue<string> q;
+        q.push(startGene);
+        unordered_set<string> visited;
+        visited.insert(startGene);
+        int steps = 0;
+        vector<char> genes = {'A','C','G','T'};
+        
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                string curr = q.front(); q.pop();
+                if (curr == endGene) return steps;
+                for (int j = 0; j < 8; j++) {
+                    char original = curr[j];
+                    
+                    for (char g : genes) {
+                        curr[j] = g;
+                        
+                        if (valid.count(curr) && !visited.count(curr)) {
+                            visited.insert(curr);
+                            q.push(curr);
+                        }
+                    }
+                    
+                    curr[j] = original; // restore
+                }
+            }
+            
+            steps++;
         }
-        return expectedSum - actualSum;
+        
+        return -1;
     }
 };
