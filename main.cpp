1cc92b765d5d4c5cd51d5248288f1c39ddaa3f7e
a01831645-cell <a01831645@tec.mx>
2026-04-20 21:43:31 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-/** File: main.cpp
- * Description: Solution for counting bits problem
- * Author: Marcel Nicklaß
- * Date: 2026-04-20
+/*
+ * Copyright (c) 2026 Marcel Nicklaß
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
  */
 class Solution {
 public:
