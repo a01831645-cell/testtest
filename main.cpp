42186c3b82019913929c9d874dde0dd0fc809314
a01831645-cell <a01831645@tec.mx>
2026-04-22 14:12:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-/*
- * Project: <your-project-name>
- * Copyright (C) 2026 Marcel Nicklaß
- * This file is part of test.
- * Licensed under the GNU General Public License v3.0 or later.
- * See <https://www.gnu.org/licenses/>.
- */
 class Solution {
 public:
     vector<int> CountBits(int n) {
