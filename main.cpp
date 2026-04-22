942527fc4ef80e92c7b29693f82637f9a8870e1f
a01831645-cell <a01831645@tec.mx>
2026-04-22 14:18:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,12 @@
-/*
- * Copyright (c) 2026 Marcel Nicklaß
- * All rights reserved.
- */
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> CountBits(int n) {
         vector<int> ans(n + 1, 0);
-        
+
         for (int i = 1; i <= n; i++) {
             ans[i] = ans[i & (i - 1)] + 1;
         }
-        
+
         return ans;
     }
 };
-
-int main() {
-    Solution s;
-    int n = 10;
-
-    vector<int> result = s.CountBits(n);
-
-    cout << "CountBits(" << n << "): ";
-    for (int x : result) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
