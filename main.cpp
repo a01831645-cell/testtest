cb4367a6a235c4e56c4f5e56c0699f8a87b83493
a01831645-cell <a01831645@tec.mx>
2026-04-23 20:23:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,12 @@
-/*
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2013 SonarSource
- * mailto:contact AT sonarsource DOT com
- *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-#include <iostream>
-#include <numeric>
-#include <cmath>
-using namespace std;
-
 class Solution {
 public:
-    int GcdOfOddEvenSums(int n) {
-        int evenNumbers, oddNumbers = 0;
-        evenNumbers = n*(n+1);
-        oddNumbers = pow(n, 2);
-        return gcd(evenNumbers, oddNumbers);
+    vector<int> CountBits(int n) {
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i & (i - 1)] + 1;
+        }
+
+        return ans;
     }
 };
